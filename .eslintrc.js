fc6a2381a16c0c915720d79c51967a3e58b0cb9f
Alex Berneau <haldir.alex@gmail.com>
2016-12-08 14:57:09 +0100

    update line break compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
       'js': 'never',
       'vue': 'never'
     }],
+    'linebreak-style': ['error', process.env.NODE_ENV === 'prod' ? 'unix' : 'windows'],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
