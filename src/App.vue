3bdf3835971ae73a15842dbb585c8809c4b81a93
ulysset <ulysse.tallepied@hetic.net>
2016-12-16 14:05:19 +0100

    fix fontface

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
     opacity: 0
   }
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Open Sans', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
